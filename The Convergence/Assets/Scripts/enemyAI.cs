36eeafb17fe1276690dc93a6c1dde563f25f9e27
Jeremy Desfosses <jeremydesfosses603@gmail.com>
2025-11-08 16:56:57 -0500

    Created Project

---
+++ The Convergence/Assets/Scripts/enemyAI.cs
@@ -0,0 +1,128 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class enemyAI : MonoBehaviour, IDamage
+{
+    [SerializeField] NavMeshAgent agent;
+    [SerializeField] Renderer model;
+    [SerializeField] Transform headPos;
+
+    [SerializeField] int HP;
+    [SerializeField] int FOV;
+    [SerializeField] int faceTargetSpeed;
+
+    [SerializeField] GameObject bullet;
+    [SerializeField] float shootRate;
+    [SerializeField] Transform shootPOS;
+
+
+    Color colorOrig;
+
+    bool playerInTrigger;
+
+    float shootTimer;
+    float angleToPlayer;
+    float stoppingDistOrig;
+
+    Vector3 playerDir;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        colorOrig = model.material.color;
+        gamemanager.instance.updateGameGoal(1);
+        stoppingDistOrig = agent.stoppingDistance;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        shootTimer += Time.deltaTime;
+
+        if (playerInTrigger && canSeePlayer())
+        {
+
+        }
+    }
+
+    bool canSeePlayer()
+    {
+        playerDir = gamemanager.instance.player.transform.position - headPos.position;
+        angleToPlayer = Vector3.Angle(playerDir, transform.forward);
+
+        Debug.DrawRay(headPos.position, playerDir);
+
+        RaycastHit hit;
+        if (Physics.Raycast(headPos.position, playerDir, out hit))
+        {
+            Debug.Log(hit.collider.name);
+
+            if (angleToPlayer <= FOV && hit.collider.CompareTag("Player"))
+            {
+                agent.SetDestination(gamemanager.instance.player.transform.position);
+
+                if (shootTimer >= shootRate)
+                {
+                    shoot();
+                }
+                if (agent.remainingDistance <= stoppingDistOrig)
+                    faceTarget();
+
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void faceTarget()
+    {
+        Quaternion rot = Quaternion.LookRotation(new Vector3(playerDir.x, 0, playerDir.z));
+        transform.rotation = Quaternion.Lerp(transform.rotation, rot, faceTargetSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInTrigger = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInTrigger = false;
+        }
+    }
+
+    public void takeDamage(int amount)
+    {
+        HP -= amount;
+
+        if (HP <= 0)
+        {
+            gamemanager.instance.updateGameGoal(-1);
+            Destroy(gameObject);
+        }
+        else
+        {
+            StartCoroutine(flashRed());
+        }
+    }
+
+    IEnumerator flashRed()
+    {
+        model.material.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        model.material.color = colorOrig;
+    }
+
+    void shoot()
+    {
+        shootTimer = 0;
+        Instantiate(bullet, shootPOS.position, transform.rotation);
+    }
+}
---
+++ The Convergence/Assets/Scripts/playerController.cs
@@ -0,0 +1,131 @@
+using UnityEngine;
+using System.Collections;
+
+public class playerController : MonoBehaviour, IDamage
+{
+    [SerializeField] CharacterController controller;
+    [SerializeField] LayerMask ignoreLayer;
+
+    [SerializeField] int HP;
+    [SerializeField] int speed;
+    [SerializeField] int sprintMod;
+    [SerializeField] int JumpSpeed;
+    [SerializeField] int maxJumps;
+    [SerializeField] int gravity;
+
+    [SerializeField] int shootDamage;
+    [SerializeField] int shootDist;
+    [SerializeField] float shootRate;
+
+
+    Vector3 moveDir;
+    Vector3 playerVel;
+
+    int jumpCount;
+    int HPOrig;
+
+    float shootTimer;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        HPOrig = HP;
+        updatePlayerUI();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * shootDist, Color.red);
+
+        shootTimer += Time.deltaTime;
+
+        movement();
+        sprint();
+    }
+
+    void movement()
+    {
+        if (controller.isGrounded)
+        {
+            playerVel = Vector3.zero;
+            jumpCount = 0;
+        }
+        else
+        {
+            playerVel.y -= gravity * Time.deltaTime;
+        }
+        moveDir = Input.GetAxis("Horizontal") * transform.right + Input.GetAxis("Vertical") * transform.forward;
+        controller.Move(moveDir * speed * Time.deltaTime);
+
+        jump();
+        controller.Move(playerVel * Time.deltaTime);
+
+        if (Input.GetButton("Fire1") && shootTimer >= shootRate)
+        {
+            shoot();
+        }
+    }
+
+    void sprint()
+    {
+        if (Input.GetButtonDown("Sprint"))
+        {
+            speed *= sprintMod;
+        }
+        else if (Input.GetButtonUp("Sprint"))
+        {
+            speed /= sprintMod;
+        }
+    }
+
+    void jump()
+    {
+        if (Input.GetButtonDown("Jump") && jumpCount < maxJumps)
+        {
+            playerVel.y = JumpSpeed;
+            jumpCount++;
+        }
+    }
+
+    void shoot()
+    {
+        shootTimer = 0;
+
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, shootDist, ~ignoreLayer))
+        {
+            Debug.Log(hit.collider.name);
+
+            IDamage dmg = hit.collider.GetComponent<IDamage>();
+            if (dmg != null)
+            {
+                dmg.takeDamage(shootDamage);
+            }
+        }
+    }
+
+    public void takeDamage(int amount)
+    {
+        HP -= amount;
+        updatePlayerUI();
+        StartCoroutine(screenFlashDamage());
+
+        if (HP <= 0)
+        {
+            // You Lose!!!
+            gamemanager.instance.youLose();
+        }
+    }
+    public void updatePlayerUI()
+    {
+        gamemanager.instance.playerHPBar.fillAmount = (float)HP / HPOrig;
+    }
+
+    IEnumerator screenFlashDamage()
+    {
+        gamemanager.instance.playerDamagePanel.SetActive(true);
+        yield return new WaitForSeconds(0.1f);
+        gamemanager.instance.playerDamagePanel.SetActive(false);
+    }
+}
