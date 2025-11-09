fb9ca0ea81961c560327a00ea5ae216f684011f2
Tomega <tomega821@gmail.com>
2025-11-08 19:53:24 -0500

    pause menu and resume button createed with gamemanager script

---
+++ The Convergence/Assets/Scripts/gamemanager.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+
+public class gamemanager : MonoBehaviour
+{
+    public static gamemanager instance;
+
+    [SerializeField] GameObject menuActive;
+    [SerializeField] GameObject menuPause;
+    [SerializeField] GameObject menuWin;
+    [SerializeField] GameObject menuLose;
+
+    public bool isPaused;
+
+    float timeScaleOrig;
+
+    int gameGoalCount;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        instance = this;
+        timeScaleOrig = Time.timeScale;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if (menuActive == null)
+            {
+                statePause();
+                menuActive = menuPause;
+                menuActive.SetActive(true);
+            }
+            else if (menuActive == menuPause)
+            {
+                stateUnpause();
+            }
+        }
+
+    }
+    public void statePause()
+    {
+        isPaused = true;
+        Time.timeScale = 0;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+    public void stateUnpause()
+    {
+        isPaused = false;
+        Time.timeScale = timeScaleOrig;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        menuActive.SetActive(false);
+        menuActive = null;
+    }
+
+}
