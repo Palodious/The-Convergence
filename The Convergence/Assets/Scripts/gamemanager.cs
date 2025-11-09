57334c831ff73150cf29ebe75dbe58e487e4210b
Tomega <tomega821@gmail.com>
2025-11-09 02:56:12 -0500

    Update gamemanager.cs

--- The Convergence/Assets/Scripts/gamemanager.cs
+++ The Convergence/Assets/Scripts/gamemanager.cs
@@ -1,6 +1,4 @@
 using UnityEngine;
-using TMPro;
-using UnityEngine.UI;
 
 public class gamemanager : MonoBehaviour
 {
@@ -10,10 +8,6 @@ public class gamemanager : MonoBehaviour
     [SerializeField] GameObject menuPause;
     [SerializeField] GameObject menuWin;
     [SerializeField] GameObject menuLose;
-
-
-    public Image playerHPBar;
-
 
     public bool isPaused;
 
