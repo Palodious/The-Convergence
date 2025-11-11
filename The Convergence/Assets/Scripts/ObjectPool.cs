4ae6a99be46aee289673cb5c8f142ab6d864d5f4
Teresa Shaver <tmshaver@student.fullsail.edu>
2025-11-10 18:24:42 -0700

    making more updates in abilities and effects
    dun dun dun, probably about half way thru

--- The Convergence/Assets/Scripts/ObjectPool.cs
+++ The Convergence/Assets/Scripts/ObjectPool.cs
@@ -1,16 +1,70 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.VFX;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class ObjectPool : MonoBehaviour
 {
+
+    public GameObject prefab;
+    public int initialSize = 10;
+    private Queue<GameObject> pool = new Queue<GameObject>();
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        for (int i = 0; i < initialSize; i++)
+        {
+            CreateNewObject();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public GameObject GetObject()
     {
-        
+        if (pool.Count == 0)
+        {
+            CreateNewObject();
+        }
+
+        GameObject obj = pool.Dequeue();
+        obj.SetActive(true);
+        return obj;
+    }
+
+    public void ReturnObject(GameObject obj)
+    {
+        obj.SetActive(false);
+        pool.Enqueue(obj);
+    }
+
+    private void CreateNewObject()
+    {
+        GameObject obj = Instantiate(prefab);
+        obj.SetActive(false);
+        pool.Enqueue(obj);
+
+        var returnToPool = obj.AddComponent<ReturnToPool>();
+        returnToPool.pool = this;
+
     }
 }
+
+public class ReturnToPool : MonoBehaviour
+{
+    public ObjectPool pool;
+    private float timer;
+
+   void OnDisable()
+    {
+        pool?.ReturnObject(gameObject);
+    }
+
+    void Update()
+    {
+        var ps = GetComponent<ParticleSystem>();
+        if (ps && !ps.IsAlive()) {
+                pool.ReturnObject(gameObject);
+        }
+    }
+}
+
