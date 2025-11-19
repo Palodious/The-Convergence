dead47ecc0f4e39439e795a670ed8995b7db62f9
Jeremy Desfosses <jeremydesfosses603@gmail.com>
2025-11-18 22:44:51 -0500

    gunStats

--- The Convergence/Assets/PreFabs/Scripts/gunStats.cs
+++ The Convergence/Assets/PreFabs/Scripts/gunStats.cs
@@ -6,8 +6,8 @@ public class gunStats : ScriptableObject
 {
     public GameObject gunModel;
 
-    [Range(1, 10)] public int shootDamage;
-    [Range(15, 1000)] public int shootDist;
+    [Range(10, 50)] public int shootDamage;
+    [Range(15, 60)] public int shootDist;
     [Range(0.1f, 2)] public float shootRate;
     public int ammoCur;
     [Range(5, 50)] public int ammoMax;
