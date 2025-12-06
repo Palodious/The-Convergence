85a2bfb3dd700fd694d43536d5035c8c93c53255
Tomega <tomega821@gmail.com>
2025-12-06 14:16:10 -0500

    turret script created

---
+++ The Convergence/Assets/Scripts/Turret.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UIElements;
+using UnityEngine.XR;
+using static UnityEngine.GraphicsBuffer;
+using static UnityEngine.ParticleSystem;
+
+public class Turret : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
