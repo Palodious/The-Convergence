81450ed1642a88cf3314ace2de7203bd9481eda4
KJEden <KJEdenPlach@student.fullsail.edu>
2025-12-07 11:33:32 -0600

    store changes

--- The Convergence/Assets/Scripts/UI/VendingMachineInteraction.cs
+++ The Convergence/Assets/Scripts/UI/VendingMachineInteraction.cs
@@ -64,13 +64,13 @@ public class VendingMachineInteraction : MonoBehaviour
         {
             Time.timeScale = 0f;
             Cursor.lockState = CursorLockMode.None;
-
+            Cursor.visible = true;
         }
         else
         {
             Time.timeScale = 1f;
             Cursor.lockState = CursorLockMode.Locked;
-
+            Cursor.visible = false;
         }
     }
     
