0f9e163eda7ca696253112d2be969023bad7c66d
Tomega <tomega821@gmail.com>
2025-12-12 22:27:54 -0500

    Update enemyAI.cs

--- The Convergence/Assets/Scripts/Enemy/enemyAI.cs
+++ The Convergence/Assets/Scripts/Enemy/enemyAI.cs
@@ -157,7 +157,7 @@ public class enemyAI : MonoBehaviour, IDamage, ISaveable
     private Vector3 lastPlayerPosition;
     private Vector3 playerVelocity;
 
-    void Awake()
+    void Start()
     {
         colorOrig = model.material.color;
         stoppingDistOrig = agent.stoppingDistance;
