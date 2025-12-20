fb8555a5ac3501ef4782107197ba8696d342acc3
Jeremy Desfosses <jeremydesfosses603@gmail.com>
2025-12-20 10:56:31 -0500

    Small adjustment

--- The Convergence/Assets/Scripts/Store And Items/RiftShards.cs
+++ The Convergence/Assets/Scripts/Store And Items/RiftShards.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class RiftShards : ICurrency
 {
     [Header("~=~=Rift Shards Amount=~=~")]
-    [SerializeField] private int amount = 0;
+    [Range(0, 999999)][SerializeField] private int amount = 0;
 
     public string Name => "Rift Shards";
 
